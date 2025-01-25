e7b880b702cd486a6258413380bd6fad09ed4670
Preethika05971 <preethisukumar0908@gmail.com>
2025-01-25 15:12:08 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,7 +5,7 @@ public class Fibonacci
   int n = 10, a = 0, b = 1;
   for (int i = 0; i < n; i++) 
   {
-    System.out.print(a + " ");
+    System.out.println(a + " ");
     a = a + b;
     b = a - b;
    }
